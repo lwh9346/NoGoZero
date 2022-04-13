79214e183d49af4166ce229c5572bdc5b87cf229
lwh9346 <lwh9346@163.com>
2022-04-13 22:41:18 +0800

    完成了游戏规则的输出和MCTS的搭建

---
+++ mcts.py
@@ -0,0 +1,112 @@
+from math import log
+import numpy as np
+
+from nogo import get_legal_actions
+
+
+class Evaluator:
+    """评估器，需要实现一个eval函数"""
+
+    def eval(self, board_A, board_B, legal_actions) -> tuple[np.ndarray, float]:
+        """输入棋盘，输出每个位置的p以及对当前局面的评价v"""
+        return np.random.random((9, 9))+.01, .0
+
+
+evaluator = Evaluator()  # 神经网络，或者什么类似的东西，用于指导MCTS
+c_puct = 0.5
+
+
+class _TreeNode:
+    """包含了W、N、父子结点以及评估结果"""
+
+    def __init__(self, parent, action, board_A=None, board_B=None) -> None:
+        self.parent = parent
+        self.w = .0
+        self.n = 0
+        if parent is None:
+            self.board_A = board_A
+            self.board_B = board_B
+        else:
+            self.board_A = np.copy(parent.board_B)
+            self.board_B = np.copy(parent.board_A)
+            x, y = action
+            self.board_B[x][y] = 1
+        self.actions = get_legal_actions(self.board_A, self.board_B)
+        self.children = [None]*len(self.actions)
+        self.p, self.v = evaluator.eval(
+            self.board_A, self.board_B, self.actions)
+
+
+def _select_and_expand_and_evaluate(t: _TreeNode) -> _TreeNode:
+    if len(t.actions) == 0:
+        return t
+    log_N = log(t.n)
+    max_qu = -2.
+    max_arg = 0
+    for i in range(len(t.children)):
+        c = t.children[i]
+        x, y = t.actions[i]
+        n = c.n if c is not None else 0
+        q = c.w/n if c is not None else 0
+        qu = q+c_puct*t.p[x][y]*log_N/(1.+n)
+        if qu > max_qu:
+            max_qu = qu
+            max_arg = i
+    if t.children[max_arg] is None:
+        nt = _TreeNode(t, t.actions[max_arg])
+        t.children[max_arg] = nt
+        return nt
+    return _select_and_expand_and_evaluate(t.children[max_arg])
+
+
+def _backup(t: _TreeNode) -> None:
+    v = t.v
+    while t is not None:
+        t.w += v
+        t.n += 1
+        v *= -1  # 交换
+        t = t.parent
+
+
+def mcts(board_A, board_B, max_N) -> np.ndarray:
+    """输入棋盘以及最大搜索次数，给出每个位置被访问次数"""
+    root = _TreeNode(None, None, board_A, board_B)
+    _backup(root)
+    for _ in range(max_N):
+        t = _select_and_expand_and_evaluate(root)
+        _backup(t)
+    result = np.zeros((9, 9))
+    for i in range(len(root.children)):
+        x, y = root.actions[i]
+        result[x][y] = root.children[i].n if root.children[i] is not None else 0
+    return result
+
+
+if __name__ == "__main__":
+    board_A = np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+    )
+    board_B = np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 1, 1, 0, 0, 0, 0],
+        ]
+    )
+    print(mcts(board_A, board_B, 1000))
---
+++ nogo.py
@@ -0,0 +1,113 @@
+import numpy as np
+from enum import Enum
+
+_cxy = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+_points = [(i//9, i % 9) for i in range(81)]
+
+
+def get_legal_actions(board_A, board_B) -> list:
+    """
+    返回A玩家可落子点集
+    board_A/board_B:9*9棋盘，1代表有子，0代表无子
+    """
+    sole_airs_B = set()
+    sole_airs_A = set()
+    not_sole_airs_A = set()
+    visited = set()
+    for x, y in _points:
+        if (x, y) in visited:
+            continue
+        if board_A[x][y]:
+            members, airs = _bfs_group(board_A, board_B, x, y)
+            for m in members:
+                visited.add(m)
+            for a in airs:
+                if len(airs) == 1:
+                    sole_airs_A.add(a)
+                else:
+                    not_sole_airs_A.add(a)
+        if board_B[x][y]:
+            members, airs = _bfs_group(board_B, board_A, x, y)
+            for m in members:
+                visited.add(m)
+            if len(airs) == 1:
+                for a in airs:
+                    sole_airs_B.add(a)
+    result = []
+    for p in _points:
+        if p in visited:
+            continue
+        if p in sole_airs_B:
+            continue
+        if p in not_sole_airs_A:
+            result.append(p)
+            continue
+        if p in sole_airs_A:
+            x, y = p
+            for dx, dy in _cxy:
+                fx, fy = x+dx, y+dy
+                if not _in_board(fx, fy):
+                    continue
+                if board_A[fx][fy] == 0 and board_B[fx][fy] == 0:
+                    result.append((x, y))
+                    break
+            continue
+        result.append(p)
+    return result
+
+
+def _bfs_group(board_A, board_B, x0, y0):
+    airs = set()
+    visited = set()
+    to_visit = [(x0, y0)]
+    i = 0
+    while i < len(to_visit):
+        x, y = to_visit[i]
+        i += 1
+        if (x, y) in visited:
+            continue
+        visited.add((x, y))
+        for dx, dy in _cxy:
+            fx, fy = x+dx, y+dy
+            if not _in_board(fx, fy):
+                continue
+            if board_A[fx][fy] and not (fx, fy) in visited:
+                to_visit.append((fx, fy))
+    for x, y in visited:
+        for dx, dy in _cxy:
+            fx, fy = x+dx, y+dy
+            if not _in_board(fx, fy):
+                continue
+            if board_A[fx][fy] == 0 and board_B[fx][fy] == 0:
+                airs.add((fx, fy))
+    return visited, airs
+
+
+def _in_board(x, y):
+    return x >= 0 and x < 9 and y >= 0 and y < 9
+
+
+if __name__ == "__main__":
+    board_A = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 0, 1, 1, 1, 1, 1],
+        [1, 1, 0, 0, 0, 1, 1, 1, 1],
+        [1, 1, 0, 0, 0, 1, 1, 1, 0],
+    ]
+    board_B = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0, 0, 0],
+    ]
+    print(get_legal_actions(board_A, board_B))
